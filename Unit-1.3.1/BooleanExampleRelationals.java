5238e47701227711581b122420422ab6ad54ea39
JakeStach <49375804+JakeStach@users.noreply.github.com>
2021-01-05 14:52:45 -0800

    Create BooleanExampleRelationals.java

---
+++ Unit-1.3.1/BooleanExampleRelationals.java
@@ -0,0 +1,19 @@
+/*
+ * Activity 1.3.1
+ * Jake Stachlewitz
+ */
+public class BooleanExampleRelationals 
+{
+  public static void main(String[] args) 
+  {
+    int a = 1;
+    int b = 8;
+    int c = 2;
+    int d = 5;
+      
+    System.out.println("b > 3: " + (b > 5));
+    System.out.println("a < 5: " + (a < 5));
+    System.out.println("c = 2: " + (c == c));
+    System.out.println("a != c: " + !(a == c));
+  }
+}
